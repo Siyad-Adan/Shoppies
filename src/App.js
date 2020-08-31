9a24c08716c98cda0b9b1cb4661176889d8cb068
Siyad Adan <siyadadan@Siyads-MacBook-Pro.local>
2020-08-31 16:02:37 -0400

    reorganized caching file structure

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { fetchData } from './api';
 import { Search, Results, Nominations } from './components';
 import checkMark from './images/checkMark.png';
 import animatedStar from './images/AnimatedStar.png';
+import useLocalStorage from './cache/cache';
 
 function App() {
 	const [ searchText, setSearchText ] = useState('');
@@ -77,42 +78,6 @@ function App() {
 			</div>
 		</div>
 	);
-}
-
-// Hook
-function useLocalStorage(key, initialValue) {
-	// State to store our value
-	// Pass initial state function to useState so logic is only executed once
-	const [ storedValue, setStoredValue ] = useState(() => {
-		try {
-			// Get from local storage by key
-			const item = window.localStorage.getItem(key);
-			// Parse stored json or if none return initialValue
-			return item ? JSON.parse(item) : initialValue;
-		} catch (error) {
-			// If error also return initialValue
-			console.log(error);
-			return initialValue;
-		}
-	});
-
-	// Return a wrapped version of useState's setter function that ...
-	// ... persists the new value to localStorage.
-	const setValue = (value) => {
-		try {
-			// Allow value to be a function so we have same API as useState
-			const valueToStore = value instanceof Function ? value(storedValue) : value;
-			// Save state
-			setStoredValue(valueToStore);
-			// Save to local storage
-			window.localStorage.setItem(key, JSON.stringify(valueToStore));
-		} catch (error) {
-			// A more advanced implementation would handle the error case
-			console.log(error);
-		}
-	};
-
-	return [ storedValue, setValue ];
 }
 
 export default App;
